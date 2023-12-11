046b393de781b15677efd9fb3f7764319e195cce
FedericoMari <mr.federicomari@gmail.com>
2023-12-11 14:03:27 -0800

    changed goal, then

--- mari-federico-final.py
+++ mari-federico-final.py
@@ -7,7 +7,7 @@
 
 '''1st Goal: Find a job-listing website and screeen scrape its contents (jobs, companies, more info, possibly contract time and location, will have to see) 
 using the directions from 'How to Automate the Boring Stuff With Python' and my youtube video source code
-    2nd Goal: Display the contents of the screen scraper within a separate terminal, not default vscode... (tkinter?)
+    2nd Goal: Request a user input for the specific company of choice, filter down even further for ONE company...
 '''
 # parses HTML or the format in which web pages are written in
 from bs4 import BeautifulSoup
@@ -18,16 +18,7 @@ import time
 from tkinter import ttk
 from tkinter import *
 
-# define the output file for the text products:
-# file = "jobs_list.txt"
-# has an issue here: 
-job_selection = True
-def job_choice():
-    while job_selection:
-        print(input("Which company looks best? "))
-        if input in str(company_name):
-            print(f"Here is your full info. for this company: {input.strip()} + {key_skills.strip()} + {more_info.strip()}")
-        
+
 #user could provide some information to specify the job 
 print("Specify the skills you are unfamiliar with...")
 # later use this variable to 
@@ -44,7 +35,7 @@ def find_jobs():
     global key_skills
     global more_info
     
-    # get specific info from a we bsite, include website url as string
+    # get specific info from a we bsite, include website url as string using the requests library
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=%22Architectural+Design%22&txtKeywords=%22Architectural+Design%22&txtLocation=').text
 
     ''' parse the html file with lxml, will edit the 'parse tree' (hierarchy of elements within the code and relationship between them) 
@@ -83,12 +74,23 @@ def find_jobs():
                     # print(f'File Saved: {index}')
                     print('')
 
+# define the output file for the text products:
+# file = "jobs_list.txt"
+# has an issue here: 
+job_selection = True
+def job_choice():
+    while job_selection:
+        user_input = input 
+        print(user_input("Which company looks best? "))
+        if user_input in str(company_name):
+            print(f"Here is your full info. for this company: {input.strip()} + {key_skills.strip()} + {more_info.strip()}")
+
 # __name__ == '__main__' will check whether the current script is being run in the program, then calls the main() func to execute the code
 if __name__ == '__main__':
     while True:
-        # call back main functions 
+        # call back main functions
         find_jobs()
-        job_choice()
+        job_choice() 
         time_wait = 10
         print(f"Waiting: {time_wait} minutes...")
         time.sleep(time_wait*60)
