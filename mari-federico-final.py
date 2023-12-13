6f834baa4b5da0feb4becc584644b70044a0e1f6
FedericoMari <mr.federicomari@gmail.com>
2023-12-13 12:29:21 -0800

    reverted back to original code

--- mari-federico-final.py
+++ mari-federico-final.py
@@ -18,23 +18,14 @@ import requests
 import os 
 import time
 
-
 #user could provide some information to specify the job 
 print("Specify the skills you are unfamiliar with...")
-# later use this variable to 
+# define unfamiliar skill variable and filter the key string word out from the parsed code
 unfamiliar_skill = input('>')
 # will filter out/remove the input provided by the user...
 print(f"Filtering out: {unfamiliar_skill}")
-# define the output file for the text products:
-file = "jobs_list.txt"
 
 def find_jobs():
-# globalize the variables to be utilized within the whole scope of the py file:
-    global jobs
-    global company_name
-    global key_skills
-    global more_info
-    
     # get specific info from a we bsite, include website url as string using the requests library
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=%22Architectural+Design%22&txtKeywords=%22Architectural+Design%22&txtLocation=').text
 
@@ -51,45 +42,26 @@ def find_jobs():
         # define a condition, to know if the jobs were recently posted or not, use the keyword 'few' to search for these recent jobs
         if 'few' in publishing_date:
             company_name = job.find('h3', class_= 'joblist-comp-name').text.replace(' ', '')
-            job_time = soup.find('li', '$0')
-            # job_pay = job.find().replace(' ', '')
-            job_location = soup.find('span', title_ = '$0')
             key_skills = job.find('span', class_ = "srp-skills").text.replace(' ', '')
             # first tag or top tag on the website format:
             # travel to header, then to h2 tag, and finally to the a tag(information is stored there w/ link for info)
             # print'href' because it is specified in a tag, this variable contains the link, print it in string datatype
             more_info = job.header.h2.a['href'].replace(' ', '')
             if unfamiliar_skill not in key_skills:
-                # creating a file name to display the text values
                     # '\n will break a line within the text file...making it more presentable within the file'
                     # could use f.write in front to write on a separate file
                     print(f"Company Name: {company_name.strip()} \n")
                     print(f"Skills Required: {key_skills.strip()}")
-                    print(f"Location: {job_location}")
-                    # print(f"Salary: {job_pay}")
-                    print(f"Length of Job: {job_time}")
                     print(f"More information: {more_info.strip()}")
-                    # provides an extra space for your displaying of information
-                    # print(f'File Saved: {index}')
-                    print('')
 
-# define the output file for the text products:
-# file = "jobs_list.txt"
-# has an issue here: 
-# job_selection = True
-# def job_choice():
-#     while job_selection:
-#         user_input = input 
-#         print(user_input("Which company looks best? "))
-#         if user_input in str(company_name):
-#             print(f"Here is your full info. for this company: {input.strip()} + {key_skills.strip()} + {more_info.strip()}")
+                    # provides an extra space for your displaying of information in between each info for the company
+                    print('')
 
 # __name__ == '__main__' will check whether the current script is being run in the program, then calls the main() func to execute the code
 if __name__ == '__main__':
     while True:
         # call back main functions
         find_jobs()
-        # job_choice() 
         time_wait = 10
         print("You may now consider your options...")
         print(f"Waiting: {time_wait} minutes...")
